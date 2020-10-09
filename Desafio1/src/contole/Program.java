4201a407fc329486063d430bc898fd3747aac174
Thiago Maia <55986447+Thiagomaiaa@users.noreply.github.com>
2020-10-09 12:46:08 -0300

    Update Program.java

--- Desafio1/src/contole/Program.java
+++ Desafio1/src/contole/Program.java
@@ -1,20 +1,21 @@
 package controle;
 import java.util.Scanner;
-import controle.MateriaPrima.MateriaPrima;
+import MateriaPrima;
 
 public class Program {
 
     public static void main(String[] args){
-        Materiaprima m1 = new Materiaprima();
+       
         Scanner leia  = new Scanner(System.in);
         System.out.print("Olá, digite o número da amostra: ");
-        m1.num = leia.nextInt();
+        int num = leia.nextInt();
         System.out.print("Olá, digite o % do conteúdo do carbono: ");
-        m1.carbono =leia.nextDouble();
+        double carbono =leia.nextDouble();
         System.out.print("Olá, digite o valor da dureza: ");
-        m1.dureza = leia.nextDouble();
+        double dureza = leia.nextDouble();
         System.out.print("Olá, digite a resistência a tração em psi: ");
-        m1.tracao = leia.nextDouble();
+        double tracao = leia.nextDouble();
+        Materiaprima m1 = new Materiaprima(num,carbono,dureza,tracao);
 
         System.out.println();
         
